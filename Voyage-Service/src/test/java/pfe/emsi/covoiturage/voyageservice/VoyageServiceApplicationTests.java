8ad77500aad84f6381ed3cfd323d872f42cc1cc7
pea5e <sindibade2011@gmail.com>
2024-12-12 22:20:27 +0100

    Relations Map

---
+++ Voyage-Service/src/test/java/pfe/emsi/covoiturage/voyageservice/VoyageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pfe.emsi.covoiturage.voyageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
