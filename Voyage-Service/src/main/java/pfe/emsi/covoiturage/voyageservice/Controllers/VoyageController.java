7adf0b6fa2c8a7da40ecc709743108ffe4ae5c7e
pea5e <sindibade2011@gmail.com>
2024-12-13 03:06:56 +0100

    Relations Map

--- Voyage-Service/src/main/java/pfe/emsi/covoiturage/voyageservice/Controllers/VoyageController.java
+++ Voyage-Service/src/main/java/pfe/emsi/covoiturage/voyageservice/Controllers/VoyageController.java
@@ -1,4 +1,29 @@
 package pfe.emsi.covoiturage.voyageservice.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pfe.emsi.covoiturage.voyageservice.Services.VoyageService;
+
+@RestController
 public class VoyageController {
+
+    private final VoyageService voyageService;
+
+    public VoyageController(VoyageService voyageService) {
+        this.voyageService = voyageService;
+    }
+
+    @PostMapping("/add")
+    public void saveVoyage(VoyageDTO voyage)
+    {
+        voyageService.saveVoyage(voyage);
+    }
+
+    @GetMapping("/get")
+    public void getVoyages()
+    {
+        voyageService.getVoyages();
+    }
 }
--- Voyage-Service/src/main/java/pfe/emsi/covoiturage/voyageservice/Services/VoyageService.java
+++ Voyage-Service/src/main/java/pfe/emsi/covoiturage/voyageservice/Services/VoyageService.java
@@ -1,12 +1,14 @@
 package pfe.emsi.covoiturage.voyageservice.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pfe.emsi.covoiturage.voyageservice.Controllers.VoyageDTO;
 import pfe.emsi.covoiturage.voyageservice.Entities.Voyage;
 import pfe.emsi.covoiturage.voyageservice.Repositories.VoyageRepo;
 
 import java.util.List;
 
+@Service
 public class VoyageService {
     private final VoyageRepo voyageRepo;
 
@@ -18,7 +20,7 @@ public class VoyageService {
         return voyageRepo.save(DTOMapping.DTOtoObject(voyageDTO));
     }
 
-    public List<Voyage> getVoyages(VoyageDTO voyageDTO){
+    public List<Voyage> getVoyages(){
         return voyageRepo.findAll();
     }
 }
