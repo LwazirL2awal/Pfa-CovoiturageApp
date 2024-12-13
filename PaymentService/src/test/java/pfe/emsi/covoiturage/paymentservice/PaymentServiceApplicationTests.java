23ddd7a9e0e7d945971b38b77923a4843a4e9b08
pea5e <sindibade2011@gmail.com>
2024-12-13 01:34:15 +0100

    Relations Map

---
+++ PaymentService/src/test/java/pfe/emsi/covoiturage/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pfe.emsi.covoiturage.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
